b267b73764984c9e7dbf85f03e9598059cd43b05
margaZM <aurazambrano227@gmail.com>
2022-01-08 14:29:24 -0500

    set up store

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,5 +2,26 @@
 
 export default () => ({
   isLoading: true,
-  entries: [1, 2, 3, 4, 5, 6]
+  entries: [
+  {
+    id: new Date().getTime(),
+    date: new Date().toDateString(),
+    text: 'Facere, cumque nisi odio similique magni fugiat aut. Facere dolores quis, ducimus ab perspiciatis explicabo velit.',
+    picture: null
+  },
+  {
+    id: new Date().getTime() * 100,
+    date: new Date().toDateString(),
+    text: 'Mollitia assumenda expedita, dolore ut, error deleniti distinctio sunt praesentium tempore accusantium, fugit quibusdam vero eum placeat voluptate.',
+    picture: null
+  },
+  {
+    id: new Date().getTime() * 200,
+    date: new Date().toDateString(),
+    text: 'Doloremque consectetur voluptates perferendis, amet consectetur adipisicing elit. Modi optio tempora asperiores odio unde.',
+    picture: null
+  },
+
+
+]
 })
