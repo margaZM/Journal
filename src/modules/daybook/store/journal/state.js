26e115350e5c14c74b972e8a6d0482e3f0363980
margaZM <aurazambrano227@gmail.com>
2022-01-12 19:12:37 -0500

    ADd info entries

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -4,24 +4,22 @@ export default () => ({
   isLoading: true,
   entries: [
   {
-    id: new Date().getTime(),
+    id: '1',
     date: new Date().toDateString(),
     text: 'Facere, cumque nisi odio similique magni fugiat aut. Facere dolores quis, ducimus ab perspiciatis explicabo velit.',
     picture: null
   },
   {
-    id: new Date().getTime() * 100,
+    id: '2',
     date: new Date().toDateString(),
     text: 'Mollitia assumenda expedita, dolore ut, error deleniti distinctio sunt praesentium tempore accusantium, fugit quibusdam vero eum placeat voluptate.',
     picture: null
   },
   {
-    id: new Date().getTime() * 200,
+    id: '3',
     date: new Date().toDateString(),
     text: 'Doloremque consectetur voluptates perferendis, amet consectetur adipisicing elit. Modi optio tempora asperiores odio unde.',
     picture: null
   },
-
-
 ]
 })
