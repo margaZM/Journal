7ff7daf969155a802b50ca92b2e97f6287b9e1b1
margaZM <aurazambrano227@gmail.com>
2022-01-12 19:08:11 -0500

    Add function get entries by term and get by id

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -4,10 +4,20 @@
 
 //Forma rapida de leer el state y traer la información como la necesito
 
-export const getEntriesByTerm = ( /*state */ ) => {
+export const getEntriesByTerm = ( state ) => ( term = '') => {
 
+  if ( term.length ===0 ) return state.entries
+  
+  return state.entries.filter( entry => entry.text.toLowerCase().includes( term.toLowerCase()
+))
 }
 
-export const getEntryById = ( /*state */ ) => {
-  
+// recibir id como argumento
+export const getEntryById = ( state ) => ( id ) => {
+
+  const entry = state.entries.find( entry => entry.id === id)
+
+  if( !entry ) return 
+
+  return {...entry} //TODO: Probar
 }
