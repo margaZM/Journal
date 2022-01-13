165f46ff42b691db5351a58f7b1db09b0cd86bba
margaZM <aurazambrano227@gmail.com>
2022-01-12 19:03:15 -0500

    Add function to get entry date

---
+++ src/modules/daybook/helpers/getDayMonthYear.js
@@ -0,0 +1,16 @@
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const days   = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
+
+
+const getDayMOnthYear = ( dateString ) => {
+  const date = new Date( dateString )
+
+  return {
+    day: date.getDate(),
+    month: months[ date.getMonth() ],
+    yearDay: `${ date.getFullYear()}, ${days[ date.getDay()]}`
+  }
+
+}
+
+export default getDayMOnthYear
