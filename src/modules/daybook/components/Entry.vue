94f88a3180d86902b88b3a85bcd89fd4cdd06298
margaZM <aurazambrano227@gmail.com>
2022-01-12 19:00:06 -0500

    Add date of entry

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,24 +1,49 @@
 <template>
   <div 
     class="entry-container mb-3 pointer p-2"
-    @click="$router.push( {name: 'entry', params: { id: 10 }})"
+    @click="$router.push( {name: 'entry', params: { id: entry.id }})"
   >
     <div class="entry-tittle d-flex">
-      <span class="text-sucess fs-5 fw-bold"> 15 </span>
-      <span class="mx-1 fs-5"> Julio</span>
-      <span class="mx-2 fw-light"> 2022, jueves </span>
+      <span class="text-sucess fs-5 fw-bold"> {{ day }} </span>
+      <span class="mx-1 fs-5"> {{ month }} </span>
+      <span class="mx-2 fw-light"> {{ yearDay }} </span>
     </div>
 
     <div class="entry-description">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit rem libero, dolorem adipisci eaque nesciunt quas, sunt, ipsam beatae nisi cupiditate aliquid voluptatem aut culpa explicabo. Doloremque dolorem dolore itaque.</p>
+      <p> {{ shortText }} </p>
     </div>
   </div>
 </template>
 
 <script>
+import getDayMonthYear from '../helpers/getDayMonthYear'
+
 
 export default ({
-
+  props: {
+    entry: {
+      type: Object,
+      required: true,
+    }
+  }, computed: {
+    shortText() {
+      return ( this.entry.text.length > 130 ) 
+      ? this.entry.text.substring(0, 130) + '...'
+      : this.entry.text
+    },
+   day() {
+     const { day } = getDayMonthYear( this.entry.date )
+     return day
+   },
+   month() {
+     const { month } = getDayMonthYear( this.entry.date )
+     return month
+   },
+   yearDay() {
+     const { yearDay } = getDayMonthYear( this.entry.date )
+     return yearDay
+   }
+  }
 })
 </script>
 
